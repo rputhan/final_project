8d2432e10fe9235668a5485114d197100c164b3c
unknown <amachreealfred@yahoo.com>
2020-07-08 20:06:32 -0500

    adding code to flask

--- static/js/players.js
+++ static/js/players.js
@@ -47,11 +47,37 @@ d3.json(url).then(function(data) {
     });
           
   });
+  let teamApoints;
+  let teamAFieldgoals; 
+  let teamBpoints;
 
-  function createteam(name,team_players){
+  function createteam1(name,team_players){
       console.log(team_players)
       var row = team_players.append("p");
       row.html(`${name}`);
+
+      var url = "http://127.0.0.1:5000/betadata";
+    
+      
+      let teamApoints;
+      let teamBpoints;
+      
+      d3.json(url).then(function(betadata) {
+      
+        var url = "http://127.0.0.1:5000/playerdata";
+
+          d3.json(url).then(function(playerdata) {
+
+          for (var i = 0; i < playerdata.length; i++) {
+            if (name == playerdata[i].player){
+              teamAFieldgoals = playerdata[i].FG * betadata.FieldGoals
+            }
+            
+          }
+
+          });
+          console.log(teamAFieldgoals)
+      });
 
   };
 
@@ -61,13 +87,13 @@ function optionChanged(team_name) {
     //run through funtions when an id is selected
     name1 = team_name;
     var team_players1 = d3.select(`#sample-metadata1`)
-    createteam(name1,team_players1);
+    createteam1(name1,team_players1,teamApoints);
   };
 
 function optionChanged2(team_name2){
     name2 = team_name2;
     var team_players2 = d3.select(`#sample-metadata2`)
-    createteam(name2,team_players2);
+    createteam2(name2,team_players2);
     
     
   };
